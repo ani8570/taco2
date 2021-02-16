e56a2f8258abd54e52d4a751f48f20ae4951c37d
ani8570 <ani8570@naver.com>
2021-02-17 00:28:20 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+import tkinter as tk
 import tensorflow as tf
-
+import calendar
 
 def Say_hello(cnt) :
     for i in range(cnt):
         print("Say hello")
 
-a= "5"
-b= 3
-print(a+ str(b))
-Say_hello(1)
 
-Say_hello(10)
+
+s = "Life is short/n Use Pytohn"
+
+root = tk.Tk()
+t = tk.Text(root, height=2,width=len("Life is short"))
+t.insert(tk.END, s)
+t.pack()
+tk.mainloop()
