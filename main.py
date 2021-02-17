1f6bac58be673a6f1c5194ad527291f5c1021e42
ani8570 <67669560+ani8570@users.noreply.github.com>
2021-02-17 16:20:52 +0900

    upload file

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
-import tkinter as tk
-import tensorflow as tf
-import calendar
+from tqdm import tqdm
+import numpy as np
+import os, glob, re
+import pandas as pd
+from util.text import text_to_sequence
 
-def Say_hello(cnt) :
-    for i in range(cnt):
-        print("Say hello")
+text_dir = glob.glob(os.path.join('./kss', '*.txt'))
+filter = "([.,!?])"
 
+metadata = pd.read_csv(text_dir[0], dtype='object', sep='|', header=None)
+print(os.path.join('./kss', '*.txt'))
+wav_dir = metadata[0].values
+text = metadata[3].values
 
+out_dir = './data'
+os.makedirs(out_dir, exist_ok=True)
+os.makedirs(os.path.join(out_dir,"text"),   exist_ok=True)
+os.makedirs(os.path.join(out_dir,"mel"),    exist_ok=True)
+os.makedirs(os.path.join(out_dir,"dec"),    exist_ok=True)
+os.makedirs(os.path.join(out_dir,"spec"),   exist_ok=True)
 
-s = "Life is short/n Use Pytohn"
-
-root = tk.Tk()
-t = tk.Text(root, height=2,width=len("Life is short"))
-t.insert(tk.END, s)
-t.pack()
-tk.mainloop()
+#text
+print('Load text')
+text_len = []
+for idx, s in enumerate(tqdm(text)):
+    sentense = re.sub(re.compile(filter), '' ,s)
+    sentense = text_to_sequence(sentense)
+    text_len.append(len(sentense))
+    text_name = 'kss-text-%05d.npy' %idx
+    np.save(os.path.join(out_dir, "text", text_name), sentense, allow_pickle=False)
---
+++ util/text.py
@@ -0,0 +1,24 @@
+from typing import Sequence
+from jamo import hangul_to_jamo
+from jamo.jamo import JAMO_LEADS_MODERN, JAMO_TAILS_MODERN, JAMO_VOWELS_MODERN
+
+PAD = '_'
+EOS = '~'
+SPACE = ' '
+JLM = "".join(JAMO_LEADS_MODERN) # 초성
+JVM = "".join(JAMO_VOWELS_MODERN) # 중성
+JTM = "".join(JAMO_TAILS_MODERN) # 종성
+VALID_CHARS = JLM + JVM + JTM + SPACE
+symbols = PAD + EOS + VALID_CHARS
+
+_symbol_to_id = {s: i for i, s in enumerate(symbols)}
+_id_to_symbol = {i: s for i, s in enumerate(symbols)}
+
+def text_to_sequence(text) :
+    sequence = []
+    if not ord(JAMO_LEADS_MODERN[0]) <= ord(text[0]) <= ord(JAMO_LEADS_MODERN[-1]) : #ord 문자 -> 정수 , chr 정수 -> 문자
+        text = ''.join(list(hangul_to_jamo(text)))
+    for s in text:
+        sequence.append(_symbol_to_id[s])
+    sequence.append(_symbol_to_id['~'])
+    return sequence
